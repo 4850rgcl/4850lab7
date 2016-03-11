375013027e40eb29580fa5bb67fe822a3e72bb6b
XCVG <xcvgvcx@gmail.com>
2016-03-10 16:21:18 -0800

    added template and parser for bookings (by day) Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,11 +5,25 @@ class Welcome extends CI_Controller {
 
     public function index()
     {
+        //load models
         $this->load->model('timetable');
-        //$this->load->model('booking');
+        $this->load->model('booking');
         
+        //define data array and define title
         $data = array();
+        $data['title'] = "Bookings";
         
+        //create bookings by days element
+        $bookings_by_days = $this->timetable->get_bookings_by_days();
+        $fragments_by_days = '';
+        foreach($bookings_by_days as $booking)
+        {            
+            //parse view fragment and add to fragments string
+            $fragments_by_days .= $this->parser->parse('single_booking',$booking, TRUE);
+        }
+        $data['bookings-day'] = $fragments_by_days;
+        
+        //parse outer template
         $this->parser->parse('welcome', $data);
 
         //$this->load->view('welcome_message');
