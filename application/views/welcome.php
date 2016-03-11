e16abb92e73e5437c3613b32a1401bd885887b56
XCVG <xcvgvcx@gmail.com>
2016-03-11 14:48:17 -0800

    new div in welcome view Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -2,25 +2,31 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>{title}</title>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <title>{title}</title>
+    </head>
 
-<body>
+    <body>
 
-<div id="container">
+    <div id="container">
 
-    <h1>Bookings by Day</h1>
-    {bookings-days}    
-    
-    <h1>Bookings by Timeslot</h1>
-    {bookings-timeslots}
-    
-    <h1>Bookings by Course</h1>
-    {bookings-courses}
+        <div id="bookings-search">
+            
+        </div>
+        
+        <div id="bookings-lists">
+            <h1>Bookings by Day</h1>
+            {bookings-days}    
 
-</div>
+            <h1>Bookings by Timeslot</h1>
+            {bookings-timeslots}
 
-</body>
+            <h1>Bookings by Course</h1>
+            {bookings-courses}
+        <div>        
+
+    </div>
+
+    </body>
 </html>
